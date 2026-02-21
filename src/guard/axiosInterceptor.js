71e73788c64cef8183259670b8e514ceee0accf4
Jirayu-Kaewnongkhoon <47939767+Jirayu-Kaewnongkhoon@users.noreply.github.com>
2026-02-21 20:01:35 +0700

    Update axiosInterceptor.js

--- src/guard/axiosInterceptor.js
+++ src/guard/axiosInterceptor.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.interceptors.request.use(config => {
 
     config.headers = { 'Content-Type': 'application/json' };
-    config.baseURL = process.env.API_URL;
+    config.baseURL = process.env.VUE_APP_API_URL;
 
     // แนบ credential (cookie) ไปพร้อมกับ request
     config.withCredentials = true;
