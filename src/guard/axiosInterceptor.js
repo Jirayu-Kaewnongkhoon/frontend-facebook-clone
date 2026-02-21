b2d143ca54fd57bb7ca0f7cf3b322252f455eb71
Jirayu-Kaewnongkhoon <47939767+Jirayu-Kaewnongkhoon@users.noreply.github.com>
2026-02-21 19:38:29 +0700

    Update axiosInterceptor.js

--- src/guard/axiosInterceptor.js
+++ src/guard/axiosInterceptor.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.interceptors.request.use(config => {
 
     config.headers = { 'Content-Type': 'application/json' };
-    config.baseURL = 'https://backend-facebook-clone.herokuapp.com';
+    config.baseURL = 'https://backend-production-94de.up.railway.app';
 
     // แนบ credential (cookie) ไปพร้อมกับ request
     config.withCredentials = true;
